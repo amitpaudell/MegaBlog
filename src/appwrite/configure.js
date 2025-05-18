16f9fea48b0964d6943a97cb75da8582e159c92a
Amit Paudel <amitpaudel789@gmail.com>
2025-05-18 17:47:54 +0545

    CRUD services

---
+++ src/appwrite/configure.js
@@ -0,0 +1,70 @@
+import { Client, Databases, Storage, ID, Query } from "appwrite";
+import conf from "../conf/config";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, {
+        title,
+        content,
+        featuredImage,
+        status,
+        userId,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateDocument(slug, { title, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.updateDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, {
+        title,
+        content,
+        featuredImage,
+        status,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteDocument(slug) {
+    try {
+      await this.databases.deleteDocument(conf.appwriteCollectionId, conf.appwriteDatabaseId, slug);
+      return true;
+    } catch (error) {
+      throw error;
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(conf.appwriteDatabaseId, conf.appwriteCollectionId, queries);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const service = new Service();
+export default service;
